a5167eac089c5017dab851fcdb34ea85333bd182
Matthew Farr <matthewtfarr@gmail.com>
2020-12-30 12:47:15 -0500

    Initiate post analysis script

---
+++ PostAnalysis/PostAnalysis.R
@@ -0,0 +1,38 @@
+#-----------#
+#-Libraries-#
+#-----------#
+
+library(coda)
+library(tidyverse)
+
+#-----------#
+#-Load data-#
+#-----------#
+
+pattern <- "chain"
+
+files <- list.files(path = "./DataAnalysis", pattern = pattern, full.names = TRUE)
+
+nc <- length(files)
+
+for(i in 1:nc){
+  load(files[i])
+}
+
+out <- mcmc.list(mget(paste0(pattern, 1:nc)))
+
+#-------------#
+#-Convergence-#
+#-------------#
+
+params <- attr(out[[1]], "dimnames")[[2]]
+
+Rhat <- gelman.diag(out[c(1:nc)][,params])
+if(all(Rhat[[1]][,1] < 1.1)){
+  print("Converged")
+}else{
+  tmp <- as.numeric(which(Rhat[[1]][,1] > 1.1))
+  print("Not converged")
+  print(params[tmp])
+  traceplot(out[c(1:nc)][,params[tmp]])
+}
