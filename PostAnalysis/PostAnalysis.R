13440c91c59612d0e2a9b04c7c0944307b035d7e
Matthew Farr <matthewtfarr@gmail.com>
2021-01-13 10:49:02 -0500

    Add paths

--- PostAnalysis/PostAnalysis.R
+++ PostAnalysis/PostAnalysis.R
@@ -9,9 +9,12 @@ library(tidyverse)
 #-Load data-#
 #-----------#
 
+#setwd("Z:/Blommel")
+
 pattern <- "chain"
 
-files <- list.files(path = "./DataAnalysis", pattern = pattern, full.names = TRUE)
+#files <- list.files(path = "./DataAnalysis", pattern = pattern, full.names = TRUE)
+files <- list.files(path = "~/Blommel/DataAnalysis", pattern = pattern, full.names = TRUE)
 
 nc <- length(files)
 
