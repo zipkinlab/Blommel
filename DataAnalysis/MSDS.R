73bef9a9f979c648c9769fe4b58eceee628fe4cf
Matthew Farr <matthewtfarr@gmail.com>
2021-02-18 23:58:21 -0500

    Update to working version after conflicts

--- DataAnalysis/MSDS.R
+++ DataAnalysis/MSDS.R
@@ -2,10 +2,6 @@
 #----Hierarchical Community Distance Sampling----#
 #------------------------------------------------#
 
-#---------------------------------#
-#-Single Species Integrated Model-#
-#---------------------------------#
-
 #----------------#
 #-Load Libraries-#
 #----------------#
@@ -24,150 +20,132 @@ load(file = "../DataFormatting/FormattedData.Rdata")
 #--------------#
 
 model.code <- nimbleCode({
-
+  
   #--------#
   #-PRIORS-#
   #--------#
-
-  #Gamma1
-  gamma1 ~ dnorm(0, 0.1)
-
-
+  
   #Overdispersion
-  r.N ~ dunif(0,10)            #Number of groups
-  omega ~ dunif(0, 1)
-  #tau <- dgamma(0.1,0.1) #ZIF overdispersion
-
-
+  r.N ~ dunif(0,10) 
+  
   #Psi
   # tau_p[s] ~ dgamma(0.1, 0.1)  #Precision
   # sig_p[s] <- 1/sqrt(tau_p[s]) #Variance
-
+  
   #Sigma
   gamma0 ~ dnorm(0, 0.01)  #Intercept parameter
-
+  gamma1 ~ dnorm(0, 0.01)
+  
   #Expected Number of Groups
   alpha0 ~ dnorm(0, 0.01)    #Intercept parameter
   alpha1 ~ dnorm(0, 0.01)    #Effect of region
-  alpah2 ~ dnorm(0, 0.01)    #Effect of migration
-
+  alpha2 ~ dnorm(0, 0.01)    #Effect of migration
+  
   #------------#
   #-LIKELIHOOD-#
   #------------#
-
+  
   #-------------------#
   #-Distance sampling-#
   #-------------------#
-
+  
   for(j in 1:nsites[1]){
-
+    
     # psi[j,s] ~ dnorm(0, tau_p[s])       #Transect effect parameter
-
+    
     #Scale parameter
     sigma[j] <- exp(gamma0 + gamma1 * region[j])
-
+    
     #Construct cell probabilities for nG cells using numerical integration
     #Sum of the area (rectangles) under the detection function
-
+    
     for(k in 1:nG){
-
+      
       #Half normal detection function at midpt (length of rectangle)
       g[k,j] <- exp(-mdpt[k]*mdpt[k]/(2*sigma[j]*sigma[j]))
-
+      
       #Detection probability for each distance class k (area of each rectangle)
       f[k,j] <- g[k,j] * v/B
-
+      
       #Conditional detection probability (scale to 1)
       fc[k,j] <- f[k,j]/pcap[j]
-
+      
     }#end k loop
-
+    
     #Detection probability at each transect (sum of rectangles)
     pcap[j] <- sum(f[1:nG,j])
-
+    
     for(t in nstart[j]:nend[j]){
-
-      #Observed population @ each t,j (N-mixture)
+      
+      #Observed population @ each t,j,s (N-mixture)
       y[t,j] ~ dbin(pcap[j], N[t,j])
-
-      #Latent Number of Groups @ each t,j
-      #Zero inflated negative binomial (poisson-gamma mixture)
+      
+      #Latent Number of Groups @ each t,j,s (negative binomial)
       N[t,j] ~ dpois(lambda.star[t,j])
-
+      
       #Expected Number of Groups
-      # lambda.star[t,j,s] <- rho[t,j,s] * lambda[t,j,s]
-      lambda.star[t,j] <- z[t,j] * lambda[t,j] * rho[t,j]
-      #zero inflated component
-      z[t,j] ~ dbern(omega)
-
+      lambda.star[t,j] <- rho[t,j] * lambda[t,j]
+      
       #Overdispersion parameter for Expected Number of Groups
       rho[t,j] ~ dgamma(r.N, r.N)
-
+      
       #Linear predictor for Expected Number of Groups
-      lambda[t,j] <- exp(alpha0 + alpha1 * region[j] + alpha2 * migration[t] + log(offset[j])) #est[j,s] # + psi[j,s])
-      #ZIF over dispersion parameter
-      #est[t,j] <- dnorm(0,tau)
-
+      lambda[t,j] <- exp(alpha0 + alpha1 * region[j] + alpha2 * migration[t] + log(offset[j])) # + psi[j,s])
+      
     }#end t loop distance sampling
-
+    
   }#end j loop distance sampling
-
+  
   #-----------------#
   #-Transect counts-#
   #-----------------#
-
+  
   for(j in (nsites[1] + 1):(nsites[1] + nsites[2])){
-
-    # psi[j,s] ~ dnorm(0, tau_p[s])       #Transect effect parameter
-
+    
     #Scale parameter
     sigma.new[j] <- exp(gamma0 + gamma1 * region[j])
-
+    
     for(k in 1:8){
-
+      
       #Half normal detection function at midpt (length of rectangle)
       g[k,j] <- exp(-mdpt[k]*mdpt[k]/(2*sigma.new[j]*sigma.new[j]))
-
+      
       #Detection probability for each distance class k (area of each rectangle)
       f[k,j] <- g[k,j] * v/B
-
+      
     }#end k loop
-
+    
     #Detection probability at each transect (sum of rectangles)
     pdet[j] <- sum(f[1:8,j])
-
+    
     for(t in nstart[j]:nend[j]){
-
+      
       #Observed population @ each t,j,s (Transect counts)
       y[t,j] ~ dbin(pdet[j], N[t,j])
-
+      
       #Latent Number of Groups @ each t,j,s (negative binomial)
       N[t,j] ~ dpois(lambda.star[t,j])
-
+      
       #Expected Number of Groups
-      # lambda.star[t,j,s] <- rho[t,j,s] * lambda[t,j,s]
-      lambda.star[t,j] <- z[t,j] * lambda[t,j]
-      z[t,j] ~ dbern(omega)
-
+      lambda.star[t,j] <- rho[t,j] * lambda[t,j]
+      
       #Overdispersion parameter for Expected Number of Groups
-      # rho[t,j,s] ~ dgamma(r.N, r.N)
-
+      rho[t,j] ~ dgamma(r.N, r.N)
+      
       #Linear predictor for Expected Number of Groups
-      lambda[t,j] <- exp(alpha0 + alpha1 * region[j] + alpha2 * migration[t] + log(offset[j])) # + psi[j,s])
-
+      lambda[t,j] <- exp(alpha0 + alpha1 * region[j] + alpha2 * migration[t] + log(offset[j]))
+      
     }#end t loop transect counts
-
+    
   }#end j loop transect counts
-
-
-
+  
   for(i in 1:nobs){
-
+    
     #Observed distance classes
     dclass[i] ~ dcat(fc[1:nG, site[i]])
-
+    
   }#end i loop
-
+  
 })
 
 #--------------#
@@ -176,49 +154,39 @@ model.code <- nimbleCode({
 
 attach(Data)
 
-constants <- list(nG = nG, v = v, B = B, mdpt = mdpt, nobs = nobs,
-                  nstart = nstart, nend = nend, nsites = nsites, nspec = nspec,
-                  site = site, spec = spec, offset = offset, region = region,
+#MTF: for each species, reset nobs, site, y, dclass
+
+constants <- list(nG = nG, v = v, B = B, mdpt = mdpt, nobs = sum(spec==1),
+                  nstart = nstart, nend = nend, nsites = nsites,
+                  site = site[spec == 1], offset = offset, region = region,
                   migration = migration)
 
-data <- list(nG = nG, v = v, site = site[spec == 8], rep = rep[spec == 8],
-             y = y[,,8], B = B, mdpt = mdpt, nobs = 3003, dclass = dclass[spec == 8], nsites = nsites,
-             nreps = nreps, gs = gs[spec == 8], offset = offset, NDVI = NDVI)
-
-#data <- list(y = y, dclass = dclass)
+data <- list(y = y[,,1], dclass = dclass[spec == 1])
 
 #----------------#
 #-Initial values-#
 #----------------#
 
-Nst <- y + 1
+#MTF: update for each species
 
-
-alpha0 <- function(){
-  alpha0 <- c(runif(1,2,3), runif(1,-0.5,0.5), runif(1,1.5,2.5), runif(1,1.5,2.5), runif(1,-1,0), runif(1,-0.5,0.5), runif(1,-0.5,0.5),
-              runif(1,2,3), runif(1,2,3), runif(1,3,4), runif(1,3,4), runif(1,-0.5,0.5), runif(1,2,3), runif(1,3,4))
-  return(alpha0)
-}
+Nst <- y[,,1] + 1
 
 #---------------#
 #-Inital values-#
 #---------------#
 
-inits <- function(){list(
-  gamma0 = runif(nspec, 4.75, 6), gamma1 = runif(1, -1, 1),
-  alpha0 = alpha0(),
-  alpha1 = runif(nspec, -1, 1),
-  alpha2 = runif(nspec, -1, 1),
-  # r.N = runif(1, 1, 2), tau_p = runif(nspec, 0, 10),
-  omega = runif(1, 0, 1),
-  N = Nst)}
+inits <- function(){list(gamma0 = runif(1, 2, 6), gamma1 = runif(1, -1, 1),
+                         alpha0 = runif(1, 0, 4), alpha1 = runif(1, -1, 1), alpha2 = runif(1, -1, 1),
+                         r.N = runif(1, 1, 2), 
+                         N = Nst)}
 
 #--------------------#
 #-Parameters to save-#
 #--------------------#
 
-params <- c('gamma0', 'gamma1', 'alpha0', 'alpha1', 'alpha2', 'omega')
-# 'r.N', 'tau_p')
+params <- c('gamma0', 'gamma1',
+            'alpha0', 'alpha1', 'alpha2',
+            'r.N')
 
 #---------------#
 #-MCMC settings-#
@@ -227,6 +195,14 @@ params <- c('gamma0', 'gamma1', 'alpha0', 'alpha1', 'alpha2', 'omega')
 model <- nimbleModel(model.code, constants = constants, data = data, inits = inits())
 
 MCMCconf <- configureMCMC(model, monitors = params)
+
+MCMCconf$removeSampler(c("alpha0", "alpha1", "alpha2"))
+
+MCMCconf$addSampler(target = c("alpha0", "alpha1", "alpha2"), type = "AF_slice")
+
+MCMCconf$removeSampler(c("gamma0", "gamma1"))
+
+MCMCconf$addSampler(target = c("gamma0", "gamma1"), type = "AF_slice")
 
 MCMC <- buildMCMC(MCMCconf)
 
@@ -243,6 +219,6 @@ out <- runMCMC(model.comp$MCMC, niter = ni, nburnin = nb, nchains = nc, thin = n
 #-Save output-#
 #-------------#
 
-ID <- paste("Spec8chain", length(list.files(pattern = "Spec8chain", full.names = FALSE)) + 1, sep="")
+ID <- paste("spec1_chain", length(list.files(pattern = "spec1_chain", full.names = FALSE)) + 1, sep="")
 assign(ID, out)
 save(list = ID, file = paste0(ID, ".Rds"))
