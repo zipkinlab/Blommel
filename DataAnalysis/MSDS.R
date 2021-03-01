efb16fb878858922a91da7ffe359ffa4101e3522
Caroline Mckenna Blommel <blommelc@dev-intel14.i>
2021-03-01 14:42:48 -0500

    species 8 integrated chains

--- DataAnalysis/MSDS.R
+++ DataAnalysis/MSDS.R
@@ -156,12 +156,12 @@ attach(Data)
 
 #MTF: for each species, reset nobs, site, y, dclass
 
-constants <- list(nG = nG, v = v, B = B, mdpt = mdpt, nobs = sum(spec==8),
+constants <- list(nG = nG, v = v, B = B, mdpt = mdpt, nobs = sum(spec==9),
                   nstart = nstart, nend = nend, nsites = nsites,
-                  site = site[spec == 8], offset = offset, region = region,
+                  site = site[spec == 9], offset = offset, region = region,
                   migration = migration)
 
-data <- list(y = y[,,8], dclass = dclass[spec == 8])
+data <- list(y = y[,,9], dclass = dclass[spec == 9])
 
 #----------------#
 #-Initial values-#
@@ -169,7 +169,7 @@ data <- list(y = y[,,8], dclass = dclass[spec == 8])
 
 #MTF: update for each species
 
-Nst <- y[,,8] + 1
+Nst <- y[,,9] + 1
 
 #---------------#
 #-Inital values-#
@@ -219,6 +219,6 @@ out <- runMCMC(model.comp$MCMC, niter = ni, nburnin = nb, nchains = nc, thin = n
 #-Save output-#
 #-------------#
 
-ID <- paste("spec8_chain", length(list.files(pattern = "spec8_chain", full.names = FALSE)) + 1, sep="")
+ID <- paste("spec9_chain", length(list.files(pattern = "spec9_chain", full.names = FALSE)) + 1, sep="")
 assign(ID, out)
 save(list = ID, file = paste0(ID, ".Rds"))
