d11fc7b62b737499d3d0a9dbd7cecd2b06bffab5
Caroline Mckenna Blommel <blommelc@dev-intel14.i>
2021-04-15 13:37:26 -0400

    changed distance class loop to reflect ccorrected transect width

--- DataAnalysis/MSDS.R
+++ DataAnalysis/MSDS.R
@@ -134,7 +134,7 @@ for(j in (nsites[1] + 1):(nsites[1] + nsites[2])){
 #Scale parameter
 sigma.new[j,s] <- exp(gamma0[s] + gamma1 * region[j])
 
-for(k in 1:8){
+for(k in 1:4){
 
 #Half normal detection function at midpt (length of rectangle)
 g[k,j,s] <- exp(-mdpt[k]*mdpt[k]/(2*sigma.new[j,s]*sigma.new[j,s]))
@@ -145,7 +145,7 @@ f[k,j,s] <- g[k,j,s] * v/B
 }#end k loop
 
 #Detection probability at each transect (sum of rectangles)
-pdet[j,s] <- sum(f[1:8,j,s])
+pdet[j,s] <- sum(f[1:4,j,s])
 
 for(t in nstart[j]:nend[j]){
 
